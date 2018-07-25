7c9e9915ee40ba6af0f5c90e5c982e46088277ec
Ulysse Hakizimana <ulysse.hakizimana@gmail.com>
2018-07-24 23:10:53 -0700

    final

---
+++ app/data/friends.js
@@ -0,0 +1,86 @@
+//array that holds all the "friends" that have taken the survey
+//initialize starting with "dummy" data
+var friendList = [
+    {
+        name: "Kristine Corbett",
+        photo: "http://68.media.tumblr.com/6cea09c793de512f267ffb4dc783f466/tumblr_mh3c4hFGPu1r9zaago1_500.gif",
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: "Ross Geller",
+        photo: "http://68.media.tumblr.com/6cea09c793de512f267ffb4dc783f466/tumblr_mh3c4hFGPu1r9zaago1_500.gif",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Edin Espinoza",
+        photo: "https://giphy.com/gifs/studiosoriginals-sexy-single-flirting-l0HlvuN8cHzsURwc0",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "George Raymond",
+        photo: "https://giphy.com/stories/poetic-justice-is-25-years-old-b821b5d7-f944",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Monica Geller",
+        photo: "https://giphy.com/gifs/friends-ross-geller-monica-YVJwi6rvE4RwI",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+]
+
+module.exports = friendList;
